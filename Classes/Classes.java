995d6c3f8e2229a55f0d5677dd48bd834a8f5b96
Andre Luiz Tavares da Silva - 125111366150 <125111366150@ulife.com.br>
2022-04-11 10:36:56 -0300

    Better SetMod()

--- Classes/Classes.java
+++ Classes/Classes.java
@@ -22,40 +22,7 @@ public class Classes{
   protected int _chaMod;
 
   private int setMod(int att){
-    if(att == 1){
-      return -5;
-    }else if(att == 2 || att == 3){
-      return -4;
-    }else if(att == 4 || att == 5){
-      return -3;
-    }else if(att == 6 || att == 7){
-      return -2;
-    }else if(att == 8 || att == 9){
-      return -1;
-    }else if(att == 10 || att == 11){
-      return 0;
-    }else if(att == 12 || att == 13){
-      return 1;
-    }else if(att == 14 || att == 15){
-      return 2;
-    }else if(att == 16 || att == 17){
-      return 3;
-    }else if(att == 18 || att == 19){
-      return 4;
-    }else if(att == 20 || att == 21){
-      return 5;
-    }else if(att == 22 || att == 23){
-      return 6;
-    }else if(att == 24 || att == 25){
-      return 7;
-    }else if(att == 26 || att == 27){
-      return 8;
-    }else if(att == 28 || att == 29){
-      return 9;
-    }else if(att == 30){
-      return 10;
-    }else
-      return -10;
+    return (att - 10)/2;
   }
 
   protected void setMods(){
